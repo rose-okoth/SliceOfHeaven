120760f8d687741f5c92311cff3aea2eafeee187
rose-okoth <okoth.rose0@gmail.com>
2021-01-10 11:48:34 +0300

    add the form to the page.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business Logic
+var sizeOne = {name: "small", price: 500};
+var sizeTwo = {name: "medium", price: 800};
+var sizeThree = {name: "large", price: 1000};
+var sizeFour = {name: "extra-large", price: 1200};
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
