b2ea1a911460442ad9f2e792087a29b7e34f65c3
rose-okoth <okoth.rose0@gmail.com>
2021-01-10 13:29:54 +0300

    add the assets folder and modifications to the form

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ var sizeTwo = {name: "medium", price: 800};
 var sizeThree = {name: "large", price: 1000};
 var sizeFour = {name: "extra-large", price: 1200};
 
+var crustOne = {name:"thin", price: 100};
+var crustTwo = {name:"flatbread", price: 150};
+var crustThree = {name:"thick", price: 200};
+var crustFour = {name:"custom", price: 400};
+
+var toppingOne = {name:"pepporoni", price: 40};
+var toppingTwo = {name:"mushrooms", price: 50};
+var toppingThree = {name:"extra-cheese", price: 30};
+var toppingFour = {name:"bacon", price: 60};
+var toppingFive = {name:"olives", price: 40};
+var toppingSix = {name:"pineapple", price: 20};
+
 
 
 
