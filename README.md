17563f58fe69997e8f7c906fdfb580010880e6a5
rose-okoth <okoth.rose0@gmail.com>
2021-01-10 20:25:59 +0300

    changes from slideshow to a photo grid portfolio

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ SliceOfHeaven Live Link: [Slice Of Heaven](https://rose-okoth.github.io/SliceOfH
 ​
 # License and Copyright Information
 ​
-Copyright(c) 2020 - Rose Okoth.  
+Copyright(c) 2021 - Rose Okoth.  
 ​
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 ​
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,4 @@
 // Business Logic
-var sizeOne = {name: "small", price: 500};
-var sizeTwo = {name: "medium", price: 800};
-var sizeThree = {name: "large", price: 1000};
-var sizeFour = {name: "extra-large", price: 1200};
-
-var crustOne = {name:"thin", price: 100};
-var crustTwo = {name:"flatbread", price: 150};
-var crustThree = {name:"thick", price: 200};
-var crustFour = {name:"custom", price: 400};
-
-var toppingOne = {name:"pepporoni", price: 40};
-var toppingTwo = {name:"mushrooms", price: 50};
-var toppingThree = {name:"extra-cheese", price: 30};
-var toppingFour = {name:"bacon", price: 60};
-var toppingFive = {name:"olives", price: 40};
-var toppingSix = {name:"pineapple", price: 20};
-
 
 
 
@@ -27,17 +10,5 @@ var toppingSix = {name:"pineapple", price: 20};
 
 
 // User Interface Logic
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slides-fade");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
 
