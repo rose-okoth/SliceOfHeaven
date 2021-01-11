69ba2050292dc39756e3a3af35de80081522c984
rose-okoth <okoth.rose0@gmail.com>
2021-01-11 08:29:59 +0300

    add the changes to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
 // Business Logic
+$(document).ready(function)() {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+}
+
+var total = 0;
+var order = [];
+
+Pizza.prototype.mySizePrice = function {
+  if(this.size === "Small") {
+    return 500;
+  } else {
+    if(this.size === "Medium") {
+      return 800;
+    } else {
+      if(this.size === "Large") {
+        return 1000;
+      } else {
+        if(this.size === "Extra-large") {
+          return 1200;
+        }
+      }
+    }
+  }
+}
+
 
 
 
